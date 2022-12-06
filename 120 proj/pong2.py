c2bb81982b5126129d3d00abd54cedd0b524be8e
BWOOD738 <85311296+BWOOD738@users.noreply.github.com>
2022-12-06 17:29:01 -0500

    Update pong2.py

--- 120 proj/pong2.py
+++ 120 proj/pong2.py
@@ -43,12 +43,34 @@ ball = pygame.Rect(335,235,BALL_RADIUS,BALL_RADIUS)
 player = pygame.Rect(680, 180, PAD_WIDTH,PAD_HEIGHT)
 player2 = pygame.Rect(12, 180, PAD_WIDTH, PAD_HEIGHT)
 
+# score 1 -> p1
+# score 2 -> p2
+score1 = 0
+score2 = 0
 
 playerspeed = 0
 player2speed = 0
 
+# Function to display the scores
+# Written by BW
+def SetText(screen, text, x, y):
+    try:
+        text = str(text)
+        font = pygame.font.SysFont("Comic Sans MS", 20)
+        text = font.render(text, True, WHITE)
+        screen.blit(text, (x, y))
+
+    except Exception as e:
+        print ("Font Error")
+        raise e 
+
+# Function to reset the ball position after a win, however it keeps the speed 
+def Reset():
+    ball.x = 325
+    ball.y = 325
 
 while True:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -77,9 +99,12 @@ while True:
     if ball.top <= 0 or ball.bottom >= height:
         speedy = (speedy * -1)
 
-    if ball.left <= 0 or ball.right >= width:
-        speedx = speedx * -1
-
+    if ball.left <= 0:
+        score2 += 1
+        Reset()
+    if ball.right >= width:
+        score1 += 1
+        Reset()
 
     ball.x += speedx
     ball.y += speedy
@@ -95,6 +120,9 @@ while True:
     pygame.draw.rect(screen, WHITE, player)
     pygame.draw.rect(screen, WHITE, player2)
     pygame.draw.ellipse(screen, WHITE, ball)
-
+    SetText(screen, str(score1),  100, 50)
+    SetText(screen, "SCORE", 300, 50)
+    SetText(screen, str(score2), 600, 50)
     pygame.display.flip()
+
     fps.tick(60)
